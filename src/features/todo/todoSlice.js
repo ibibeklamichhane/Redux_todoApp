c68d04eba92fb7cce1a03d0597259d58df2122b2
ibibeklamichhane <bibeklc714@gmail.com>
2024-03-31 22:35:24 +0545

    Feat:added the functionality of adding the todos and displaying the todos

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,31 @@
+import {createSlice,nanoid} from '@reduxjs/toolkit';
+
+//initially what will be the store value will be
+const initialState = {
+    todos:[{id:1,text:"Hello  World"}]
+}
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers:{
+        addTodo:(state,action) => {
+            const todo = {
+                id:nanoid(),
+                text:action.payload
+            }
+            state.todos.push(todo)
+
+        },
+        removeTodo : (state,action) => {
+            state.todos = state.todos.filter((todo) => todo.id!== action.payload) //id will be sent and if the sent id by payload matched it is filtered and then all remaining are shown
+        },
+    }
+ 
+})
+
+export const{addTodo,removeTodo} = todoSlice.actions;
+
+export default todoSlice.reducer
